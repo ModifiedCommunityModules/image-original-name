d19be59936679ebc1ab59b6c28cf7032da4d7503
Gulliver72 <31593252+Gulliver72@users.noreply.github.com>
2020-10-02 20:41:53 +0200

    Update and rename mf_catImageOriginalName.php to mcm_catImageOriginalName.php

--- new_files/lang/english/modules/categories/mcm_catImageOriginalName.php
+++ new_files/lang/english/modules/categories/mcm_catImageOriginalName.php
@@ -1,5 +1,5 @@
 <?php
-$rthPrefix = 'MODULE_CATEGORIES' . '_MF_IMAGEORIGINALNAME_';
+$rthPrefix = 'MODULE_CATEGORIES' . '_MCM_IMAGEORIGINALNAME_';
 
 define($rthPrefix . 'TITLE', 'Get image name on upload by <a href="https://www.modified-shop.org/forum/index.php?topic=15464.0" target="_blank" style="color: #4DCDFF; font-weight: bold;">xt_modding</a>');
 define($rthPrefix . 'LONG_DESCRIPTION', 'The original image filename is preserved (for categories and products) and the names are cleaned and SEO fit. All letters are written in lower case, spaces are replaced with hyphens and umlauts etc. are cleaned up. Furthermore there is no 0 at the end of the first image. For further images the counter 1,2 etc. is appended with hyphens. Attention: These changes are not applied to already existing images! They may have to be adjusted separately.');
