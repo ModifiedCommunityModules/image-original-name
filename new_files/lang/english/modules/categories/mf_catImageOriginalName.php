f1ddcd1c0538d1762b06d35a2535f27b08d5b6ee
Gulliver72 <31593252+Gulliver72@users.noreply.github.com>
2020-10-02 20:05:02 +0200

    Create mf_catImageOriginalName.php

---
+++ new_files/lang/english/modules/categories/mf_catImageOriginalName.php
@@ -0,0 +1,7 @@
+<?php
+$rthPrefix = 'MODULE_CATEGORIES' . '_MF_IMAGEORIGINALNAME_';
+
+define($rthPrefix . 'TITLE', 'Get image name on upload by <a href="https://www.modified-shop.org/forum/index.php?topic=15464.0" target="_blank" style="color: #4DCDFF; font-weight: bold;">xt_modding</a>');
+define($rthPrefix . 'LONG_DESCRIPTION', 'The original image filename is preserved (for categories and products) and the names are cleaned and SEO fit. All letters are written in lower case, spaces are replaced with hyphens and umlauts etc. are cleaned up. Furthermore there is no 0 at the end of the first image. For further images the counter 1,2 etc. is appended with hyphens.');
+define($rthPrefix . 'STATUS_TITLE', 'Get image name on upload Activate module?');
+define($rthPrefix . 'STATUS_DESC', '');
