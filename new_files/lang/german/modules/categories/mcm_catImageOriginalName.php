e3b2cd77d0554aef6d70bf8020c79521449f647c
Gulliver72 <31593252+Gulliver72@users.noreply.github.com>
2020-10-02 20:42:34 +0200

    Update and rename mf_catImageOriginalName.php to mcm_catImageOriginalName.php

--- new_files/lang/german/modules/categories/mcm_catImageOriginalName.php
+++ new_files/lang/german/modules/categories/mcm_catImageOriginalName.php
@@ -1,5 +1,5 @@
 <?php
-$rthPrefix = 'MODULE_CATEGORIES' . '_MF_IMAGEORIGINALNAME_';
+$rthPrefix = 'MODULE_CATEGORIES' . '_MCM_IMAGEORIGINALNAME_';
 
 define($rthPrefix . 'TITLE', 'Bildernamen bei Upload erhalten by <a href="https://www.modified-shop.org/forum/index.php?topic=15464.0" target="_blank" style="color: #4DCDFF; font-weight: bold;">xt_modding</a>');
 define($rthPrefix . 'LONG_DESCRIPTION', 'Der original Bild-Dateiname wird erhalten (für Kategorien und Produkte) und die Namen bereinigt und SEO Fit gemacht. Dabei werden alle Buchstaben klein geschrieben, Leerzeichen mit Bindestrichen ersetzt und Umlaute etc bereinigt. Ferner ist beim ersten Bild keine 0 am Ende. Bei weiteren Bildern wird der Zähler 1,2 etc. mit Bindestrichen angehangen. Achtung: Diese Änderungen werden nicht bei bereits vorhandenen Bildern durchgeführt! Diese müssen gegebenfalls extra angepasst werden.');
